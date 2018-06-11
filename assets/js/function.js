4a389d18c2caf8f0f8fc3064d9b7dedc840a33c2
RootIsNameMy <30761293+RootIsNameMy@users.noreply.github.com>
2018-06-11 18:08:28 +0200

    Add files via upload

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,164 +1,189 @@
-var logoColorElem=document.getElementById("logo-color");
-var logoColors=["rgba(255,255,255,0.5)","#CA312B","#C1C800","#3ea0b5"]
-
-var logoElem=document.getElementById("logo");
-var asElem=document.getElementById("as");
-
-var randomLogo=0;
-var randomAS=0;
-function HoverLogo(hover)
-{
-    if(hover)
+$(window).scroll(function(){
+	
+    if (Math.round($(window).scrollTop())+2 > Math.round($(document).height()-$(window).height())){
+     
+		
+    }
+     else if($(window).scrollTop()<$(document).height()-$(window).height()){
+       
+	
+    }
+   
+    var h=window.innerHeight;
+    
+    
+    var scrollVar=$(window).scrollTop();
+    var visableLogo=0;
+    var treshold=50;
+    var declineLogo=0;
+    
+    
+    if(scrollVar>visableLogo+treshold)
         {
-            
-            randomLogo+=Math.floor(Math.random()*2+1);
-                    randomLogo=randomLogo%3;
-                    if(randomLogo==0)
-                        {
-                            randomLogo=3;
-                        }
-            logoColorElem.style.fill= logoColors[randomLogo];
-                    //logoElem.style.transform="scale(1.1)";
+            declineLogo=Math.abs(scrollVar-visableLogo-treshold);
+        }
+    else if(scrollVar<visableLogo-treshold)
+        {
+            declineLogo=Math.abs(scrollVar-visableLogo+treshold);
+        }
+    
+    if(declineLogo<h/2)
+        {
             
         
+    logoRight0.style.left=declineLogo+"px";
+    logoLeft0.style.right=declineLogo+"px";
+    
+    var fadeLogo=1-Math.max(0,Math.min(declineLogo*3/h,1));
+    logoRightFade0.style.fill="rgba(255,255,255,"+fadeLogo+")";
+    logoLeftFade0.style.fill="rgba(255,255,255,"+fadeLogo+")";
         }
-    else
+    var declineLogo=0;
+    visableLogo+=h;
+    if(scrollVar>visableLogo+treshold)
         {
-            logoColorElem.style.fill= logoColors[0];
-            //logoElem.style.transform="scale(1)";
-         
+            declineLogo=Math.abs(scrollVar-visableLogo-treshold);
         }
-}
-var asStudioElems=document.getElementsByClassName("studio");
-function ASHover(hover)
-{
-    if(hover)
+    else if(scrollVar<visableLogo-treshold)
         {
-            
-            randomAS+=Math.floor(Math.random()*2+1);
-                    randomAS=randomAS%3;
-                    if(randomAS==0)
-                        {
-                            randomAS=3;
-                        }
-            for(i=0;i<asStudioElems.length;i++)
-                {
-                    asStudioElems[i].style.fill=logoColors[randomAS];
-                }
-            
-        
+            declineLogo=Math.abs(scrollVar-visableLogo+treshold);
         }
-        
-    else
+     if(declineLogo<h/2)
         {
-           for(i=0;i<asStudioElems.length;i++)
-                {
-                    asStudioElems[i].style.fill=logoColors[0];
-                } 
+    logoRight1.style.left=declineLogo+"px";
+    logoLeft1.style.right=declineLogo+"px";
+    
+     fadeLogo=1-Math.max(0,Math.min(declineLogo*3/h,1));
+    logoRightFade1.style.fill="rgba(255,255,255,"+fadeLogo+")";
+    logoLeftFade1.style.fill=blueRGB+fadeLogo+")";
         }
-}
-
-
-function MenuNav(menu)// 0-home,,1-about us,2-team,3-projects,4-contact
-{
-    if(menu<4)
+    var declineLogo=0;
+    visableLogo+=h;
+    if(scrollVar>visableLogo+treshold)
         {
-    pageNav=menu;
-  zoom=!zoom;
+            declineLogo=Math.abs(scrollVar-visableLogo-treshold);
         }
-    else//contact menu
+    else if(scrollVar<visableLogo-treshold)
         {
-            
+            declineLogo=Math.abs(scrollVar-visableLogo+treshold);
         }
-}
-var zoom=false;
-var blur=true;
-var pageNav=0;
-
-document.addEventListener("scroll", function(e){
-    var y=window.pageYOffset;
-    console.write(document.location.href);
+     if(declineLogo<h/2)
+        {
+    logoRight2.style.left=declineLogo+"px";
+    logoLeft2.style.right=declineLogo+"px";
+    
+     fadeLogo=1-Math.max(0,Math.min(declineLogo*3/h,1));
+    logoRightFade2.style.fill="rgba(255,255,255,"+fadeLogo+")";
+    logoLeftFade2.style.fill="rgba(236,161,55,"+fadeLogo+")";
+        }
+    var declineLogo=0;
+    visableLogo+=h;
+    if(scrollVar>visableLogo+treshold)
+        {
+            declineLogo=Math.abs(scrollVar-visableLogo-treshold);
+        }
+    else if(scrollVar<visableLogo-treshold)
+        {
+            declineLogo=Math.abs(scrollVar-visableLogo+treshold);
+        }
+     if(declineLogo<h/2)
+        {
+    logoRight3.style.left=declineLogo+"px";
+    logoLeft3.style.right=declineLogo+"px";
+    
+     fadeLogo=1-Math.max(0,Math.min(declineLogo*3/h,1));
+    logoRightFade3.style.fill="rgba(255,255,255,"+fadeLogo+")";
+    logoLeftFade3.style.fill="rgba(120,255,120,"+fadeLogo+")";
+        }
 });
+var logoColor="#3ea1b7";
+var blueRGB="rgba(62,161,183,";
+var headerElem=document.getElementById("header");
+var logoLeft0=document.getElementById("logo-left");
+var logoRight0=document.getElementById("logo-right");
+var logoLeftFade0=document.getElementById("logo-color");
+var logoRightFade0=document.getElementById("logo-fade");
+
+var logoLeft1=document.getElementById("logo-left-1");
+var logoRight1=document.getElementById("logo-right-1");
+var logoLeftFade1=document.getElementById("logo-left-c-1");
+var logoRightFade1=document.getElementById("logo-right-c-1");
+
+var logoLeft2=document.getElementById("logo-left-2");
+var logoRight2=document.getElementById("logo-right-2");
+var logoLeftFade2=document.getElementById("logo-left-c-2");
+var logoRightFade2=document.getElementById("logo-right-c-2");
+
+var logoLeft3=document.getElementById("logo-left-3");
+var logoRight3=document.getElementById("logo-right-3");
+var logoLeftFade3=document.getElementById("logo-left-c-3");
+var logoRightFade3=document.getElementById("logo-right-c-3");
 
 
 
-var bodyElem=document.getElementById("body");
-var bodyBlur=15;
-var bodyScale=35;
-var bodyContrast=0;
-
-var siteFile="../";
-var pages=[siteFile,siteFile+"about_us",siteFile+"our_team",siteFile+"projects"];
 
 
-function goToPage(str)
+
+function OpenHead()
 {
-    window.location.assign(pages[str]);
+    
+}
+function CloseHead()
+{
     
 }
 
-var frameElem=setInterval(frame,10);
 
+
+function OpenPage1()
+{
+    
+}
+function ClosePage1()
+{
+    
+}
+
+
+
+function OpenPage2()
+{
+    
+}
+function ClosePage2()
+{
+    
+}
+
+
+
+function OpenPage3()
+{
+    
+}
+function ClosePage3()
+{
+    
+}
+
+
+
+
+var id=setInterval(frame,5);
 function frame()
 {
-    if(blur)
-       {
-            if(bodyBlur>0||bodyScale>1||bodyContrast<100)
-                {
-                    bodyBlur-=0.2;
-                    bodyScale-=0.5;
-                    bodyContrast+=2;
-                    if(bodyBlur<=0)
-                       {
-                           bodyBlur=0;
-                           
-                       }
-                    if(bodyScale<=1)
-                        {
-                            bodyScale=1;
-                        }
-                    if(bodyContrast>=100)
-                        {
-                            bodyContrast=100;
-                        }
-                    if(bodyScale==1&&bodyBlur==0&&bodyContrast==100)
-                        {
-                            blur=false;
-                        }
-                    bodyElem.style.filter="blur("+bodyBlur+"px)";
-                    bodyElem.style.transform="scale("+bodyScale+")";
-                    bodyElem.style.filter="opacity("+bodyContrast+"%)";
-                }
-           
-       }
-   else if(zoom)
-       {
-           if(bodyBlur<15||bodyContrast>0||bodyScale<35)
-               {
-                   bodyScale+=0.5;
-                   bodyBlur+=0.2;
-                   bodyContrast-=2;
-                   if(bodyBlur>=15)
-                       {
-                           bodyBlur=15;
-                           
-                       }
-                   if(bodyContrast<=0)
-                       {
-                           bodyContrast=0;
-                       }
-                   if(bodyScale>=35)
-                       {
-                           bodyScale=35;
-                       }
-                   if(bodyBlur==15&&bodyContrast==0&&bodyScale==35)
-                       {
-                           goToPage(pageNav);
-                       }
-                   bodyElem.style.filter="blur("+bodyBlur+"px)";
-                   bodyElem.style.transform="scale("+bodyScale+")";
-                   bodyElem.style.filter="opacity("+bodyContrast+"%)";
-               }
-       }
     
+}
+
+function lerpColor(a, b, amount) { 
+
+    var ah = parseInt(a.replace(/#/g, ''), 16),
+        ar = ah >> 16, ag = ah >> 8 & 0xff, ab = ah & 0xff,
+        bh = parseInt(b.replace(/#/g, ''), 16),
+        br = bh >> 16, bg = bh >> 8 & 0xff, bb = bh & 0xff,
+        rr = ar + amount * (br - ar),
+        rg = ag + amount * (bg - ag),
+        rb = ab + amount * (bb - ab);
+
+    return '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1);
 }
